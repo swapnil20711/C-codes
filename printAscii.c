e1effc1ac3a479c169348cf36250fd52e61d5be7
shubham <swapnil.bhojwani@gmail.com>
2019-12-20 16:07:42 +0530

    first commit

---
+++ printAscii.c
@@ -0,0 +1,10 @@
+// To print ascii value of given character
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character to find it's ASCII value: ");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d",ch,ch);
+    return 0;
+}
