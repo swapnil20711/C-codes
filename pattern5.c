6f394d65c0c40442e5aaf0970cc0880616c5606e
shubham <swapnil.bhojwani@gmail.com>
2019-12-21 22:22:51 +0530

    first commit

---
+++ pattern5.c
@@ -0,0 +1,22 @@
+// To print like this
+/* 1
+   22
+   333
+   4444
+   55555  */
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
