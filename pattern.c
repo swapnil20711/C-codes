30fbb3715b9c80d867fa76751fd1b1f0179898b4
shubham <swapnil.bhojwani@gmail.com>
2019-12-21 21:53:02 +0530

    first commit

---
+++ pattern.c
@@ -0,0 +1,20 @@
+/*  *
+    **
+    ***
+    **** program to print this pattern */
+#include<stdio.h>
+int main()
+{
+    int n;
+	printf("Enter the number of rows\n");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+        }
+}
+
