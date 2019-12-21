f15ef1f1a3d6ff81fab3f0fa56813e37769b5e05
shubham <swapnil.bhojwani@gmail.com>
2019-12-21 22:27:52 +0530

    first commit

---
+++ array.c
@@ -0,0 +1,17 @@
+/* NAME= ARRAY PROGRAM
+   AUTHOR= SWAPNIL BHOJWANI
+   DESCRIPTION= TO PRINT THE ARRAY ELEMENT     */
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int arr[5],i;
+    printf("Enter five numbers:");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf(" Your 2 element is %d",arr[1]);
+    return 0;
+
+}
