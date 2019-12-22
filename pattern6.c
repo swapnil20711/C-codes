6d94fad799f72e756d3daf0791bacc760b562a75
shubham <swapnil.bhojwani@gmail.com>
2019-12-22 15:17:01 +0530

    first commit

---
+++ pattern6.c
@@ -0,0 +1,17 @@
+// To print square pattern of n*n
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
