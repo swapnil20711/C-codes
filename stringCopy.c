bb66a6f49a0c806871937693ff60ffca74f030df
shubham <swapnil.bhojwani@gmail.com>
2019-12-30 21:43:24 +0530

    to copy string into another string

---
+++ stringCopy.c
@@ -0,0 +1,12 @@
+// To copy one string to another string
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100],b[100];
+    printf("Enter the string:");
+    scanf("%s",&a);
+    strcpy(b,a);
+    printf("Copied string is %s",b);
+    return 0;
+}
